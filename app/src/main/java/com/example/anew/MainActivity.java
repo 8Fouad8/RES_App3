3d506b071896c4af102ce4891a5c0f7f314e2a95
zyad246 <47024065+zyad246@users.noreply.github.com>
2022-12-21 13:40:03 +0200

    database

--- app/src/main/java/com/example/anew/MainActivity.java
+++ app/src/main/java/com/example/anew/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.database.DatabaseReference;
@@ -20,7 +21,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
+
 
     public void signin(View view) {
         Intent sign=new Intent(this,MainActivity2.class);
--- app/src/main/java/com/example/anew/MainActivity2.java
+++ app/src/main/java/com/example/anew/MainActivity2.java
@@ -1,30 +1,38 @@
 package com.example.anew;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.Debug;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity2 extends AppCompatActivity {
      Button button3;
+     EditText name = findViewById(R.id.editTextTextEmailAddress);
+    EditText password = findViewById(R.id.editTextTextPassword) ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Tester","on create");
         setContentView(R.layout.activity_main2);
           button3=findViewById(R.id.button3);
 
-        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference mDbRef = mDatabase.getReference("message");
-        button3.setOnClickListener(v -> {
-            Intent ss=new Intent(this,selectitems.class);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
-            mDbRef.setValue("Rahul");
-            startActivity(ss);
+        button3.setOnClickListener(v -> {
+
+            myRef.setValue("Hello, World!");
+
         });
 
     }
@@ -39,7 +47,8 @@ public class MainActivity2 extends AppCompatActivity {
         startActivity(swe);
     }
 
-   /* public void tomenu(View view) {
-
-    }*/
+   public void tomenu(View view) {
+       Intent ss=new Intent(this,selectitems.class);
+         startActivity(ss);
+    }
 }
